00077ff0c99f6ab4348498f38694a638b491397f
Marcelo Sanches <sanches.cello@gmail.com>
2019-07-14 17:07:13 -0600

    Update Rcode.R

--- Rcode.R
+++ Rcode.R
@@ -1,13 +1,7 @@
 ## ----setup, include=FALSE------------------------------------------------
 knitr::opts_chunk$set(echo = TRUE)
-#library(knitr)
-#purl("Titanic_Survival.Rmd", output = "Titanic_Survival.R")
 
-## ----include=FALSE-------------------------------------------------------
-#library(knitr)
-#purl("Titanic_Survival.Rmd", output = "survival.R")
-
-## ----echo=FALSE----------------------------------------------------------
+## ------------------------------------------------------------------------
 # Load training set 
 rm(list=ls()) 
 train <- read.csv("../input/train.csv", na.strings="")
@@ -23,7 +17,7 @@ head(train)
 ## ------------------------------------------------------------------------
 tail(train)
 
-## ----include=FALSE-------------------------------------------------------
+## ------------------------------------------------------------------------
 # PassengerId: Trust but Verify
 sum(duplicated(train$PassengerId)) == 0 # no duplicates
 sum(train$PassengerId == 1:891) == 891 # stepwise values
@@ -67,7 +61,7 @@ train$Title <- factor(train$Title)
 # unique titles
 unique(train$Title)
 
-## ----echo=FALSE----------------------------------------------------------
+## ------------------------------------------------------------------------
 # create NameLength attribute
 train$NameLength <- vector("numeric", nrow(train))
 for (i in 1:nrow(train)) {
@@ -123,7 +117,8 @@ temp_dfm[1:10,]
 
 ## ------------------------------------------------------------------------
 # create TicketCount
-train$TicketCount <- apply(X=train, MARGIN=1, FUN=function(dfm) counts[which(counts[,1] == dfm["Ticket"]), 2])
+train$TicketCount <- apply(X=train, MARGIN=1, 
+                          FUN=function(dfm) counts[which(counts[,1] == dfm["Ticket"]), 2])
 # drop Fare, Name, and Ticket
 '%ni%' <- Negate('%in%')
 not_chosen <- c("Fare","Name","Ticket")
@@ -136,7 +131,7 @@ train$FarePerPersonLog <- log(train$FarePerPerson+1)
 ## ------------------------------------------------------------------------
 names(train)
 
-## ----echo=FALSE----------------------------------------------------------
+## ------------------------------------------------------------------------
 # Cleaning up Cabin
 train$CabinClean <- vector("character", nrow(train))
 for (i in 1:nrow(train)) {
@@ -161,7 +156,7 @@ train$Embarked <- ifelse(train$Embarked=="C","Cherbourg",
 train$Embarked[is.na(train$Embarked)] <- "Southhampton"
 train$Embarked <- factor(train$Embarked) # re-factoring
 
-## ----fig.height=4.5, fig.width=9, echo=FALSE-----------------------------
+## ----fig.height=4.5, fig.width=9-----------------------------------------
 par(mfrow=c(1,2))
 hist(train$Age, xlab='Age', main="Before Imputation", ylab="", 
      col=rgb(0,0.4,0.4,0.4), ylim=c(0,420))
@@ -240,7 +235,7 @@ rf_age <- randomForest(Age ~., data=yesAge, mtry=2, na.action=na.omit)
 train$Age[is.na(train$Age)] <- round(predict(rf_age, newdata=noAge),0)
 sum(is.na(train$Age)) == 0
 
-## ----fig.height=4.5, fig.width=9, echo=FALSE-----------------------------
+## ----fig.height=4.5, fig.width=9-----------------------------------------
 par(mfrow=c(1,2))
 hist(train$Age, xlab='Age', main="After Random Forest Imputation", ylab="", 
      col=rgb(0,0.4,0.4,0.4), ylim=c(0,420))
@@ -272,22 +267,22 @@ summary(train)
 ## ------------------------------------------------------------------------
 names(train)
 
-## ----fig.height=5, fig.width=9, echo=FALSE-------------------------------
+## ----fig.height=5, fig.width=9-------------------------------------------
 par(mfrow=c(1,2)); par(oma=c(3,1,1,1))
 plot(train$Cabin, main="Cabin", col=terrain.colors(6))
 plot(train$Embarked, main="Port Embarked", col=terrain.colors(3), cex.axis=0.8, cex.sub=0.8, las=2)
 
-## ----fig.height=5, fig.width=9, echo=FALSE-------------------------------
+## ----fig.height=5, fig.width=9-------------------------------------------
 par(mfrow=c(1,2)); par(oma=c(3,1,1,1))
 plot(train$PclassFac, main="Pclass Categorical", col=terrain.colors(3),cex.axis=0.8, las=2)
 hist(train$NameLength, main="Name Lengths", xlab="Length (characters)", col="steelblue")
 
-## ----fig.height=5, fig.width=9, echo=FALSE-------------------------------
+## ----fig.height=5, fig.width=9-------------------------------------------
 par(mfrow=c(1,2)); par(oma=c(3,1,1,1))
 plot(train$Title, main="Titles", col=terrain.colors(5),cex.axis=0.8, las=2)
 plot(train$GenderFac, main="Sex", col=c("palevioletred1","cadetblue2"))
 
-## ----fig.height=5, fig.width=9, echo=FALSE-------------------------------
+## ----fig.height=5, fig.width=9-------------------------------------------
 SS <- table(train$SiblingSpouse)
 PC <- table(train$ParentChildren)
 counts <- rbind(SS,PC)
@@ -297,7 +292,7 @@ barplot(counts, main="Number of Siblings/Spouses vs Parents/Children",
   xlab="Number of Relatives", ylab="", col=c(rgb(0.2,0.4,0,0.3),rgb(0.2,0,0.5,0.3)),
   legend = rownames(counts), beside=TRUE)
 
-## ----fig.height=5, fig.width=9, echo=FALSE-------------------------------
+## ----fig.height=5, fig.width=9-------------------------------------------
 par(mfrow=c(1,2))
 barplot(table(train$NumRelatives), main="Total Number of Relatives",
   xlab="Number of Relatives", ylab="", col=rgb(0.4,0.1,0.2,0.3))
@@ -305,19 +300,19 @@ barplot(table(train$NumRelatives), main="Total Number of Relatives",
 barplot(table(train$TicketCount), main="Group Tickets",
   xlab="Number of People per Ticket", ylab="", col=rgb(0,0.1,0.9,0.3))
 
-## ----fig.height=5, fig.width=9, echo=FALSE-------------------------------
+## ----fig.height=5, fig.width=9-------------------------------------------
 par(mfrow=c(1,1))
 hist(train$FarePerPerson, xlab="Fare (Pounds Sterling)", ylab="", main="Fares", col=rgb(1,0,0,0.3))
 multiplication_factor <- max(train$FarePerPerson)/max(train$FarePerPersonLog)
 hist(train$FarePerPersonLog*multiplication_factor, col=rgb(0,0,1,0.2), ylab="", add=TRUE)
 legend(150, 450, pch=15, col=c(rgb(1,0,0,0.4),rgb(0,0,1,0.5)), c("Fare","Log(Fare) scaled up"))
 
-## ----fig.height=8, fig.width=9, echo=FALSE-------------------------------
+## ----fig.height=8, fig.width=9-------------------------------------------
 par(mfrow=c(2,1))
 hist(train$AgeNum, xlab='Age (yrs)', main="Age Distribution", ylab="", col=terrain.colors(8))
 barplot(table(train$AgeFac), xlab='Age Group', ylab="", col=terrain.colors(5), space=c(0,0,0,0,0))
 
-## ----fig.height=4.5, fig.width=4.5, echo=FALSE---------------------------
+## ----fig.height=4.5, fig.width=4.5---------------------------------------
 par(mfrow=c(1,1))
 # Survived
 plot(train$SurvivedFac, main="Survived", col=c("red","chartreuse3"))
@@ -335,7 +330,7 @@ tail(t(data.frame(combn(12, 2))))
 chosen <- c("SurvivedFac", "PclassNum", "GenderFac","AgeNum","FarePerPerson","Embarked")
 plot(train[,colnames(train) %in% chosen])
 
-## ----fig.height=5, fig.width=8.5, echo=FALSE, message=FALSE, warning=FALSE----
+## ----fig.height=5, fig.width=8.5, message=FALSE, warning=FALSE-----------
 # 1 Survived & Cabin
 suppressMessages(library(ggplot2))
 suppressMessages(library(ggmosaic))
@@ -345,28 +340,28 @@ ggplot(data=train2) +
    geom_mosaic(aes(x=product(Survival2, Cabin),fill=Survival2)) +
    labs(x='Cabin', y='', title='Tianic Survival by Cabin')
 
-## ----fig.height=5, fig.width=8.5, echo=FALSE-----------------------------
+## ----fig.height=5, fig.width=8.5-----------------------------------------
 # 2 Survived & Embarked 
 ggplot(data=train) +
    geom_mosaic(aes(x=product(SurvivedFac, Embarked),fill=SurvivedFac )) +
    labs(x='Port of Embarkation', y='', 
    title='Tianic Survival by Port of Embarkation')
 
-## ----fig.height=5, fig.width=9, echo=FALSE-------------------------------
+## ----fig.height=5, fig.width=9-------------------------------------------
 # Survived & Embarked & Fare
 ggplot(data=train) +
    geom_boxplot(aes(x=Embarked,y=FarePerPerson, fill=SurvivedFac)) +
    labs(x='Port of Embarkation', y='Fare (Pounds Sterling)', 
    title='Titanic Survival by Port of Embarkation and Fare')
 
-## ----fig.height=5, fig.width=9, echo=FALSE-------------------------------
+## ----fig.height=5, fig.width=9-------------------------------------------
 # Survived & Embarked & Fare
 ggplot(data=train) +
    geom_boxplot(aes(x=Embarked,y=FarePerPersonLog, fill=SurvivedFac)) +
    labs(x='Port of Embarkation', y='Fare (Log of Pounds Sterling)', 
    title='Titanic Survival by Port of Embarkation and Fare')
 
-## ----fig.height=4, fig.width=7, echo=FALSE, message=FALSE----------------
+## ----fig.height=4, fig.width=7, message=FALSE----------------------------
 # 3 Survived and Pclass 
 ggplot(data=train) +
    geom_mosaic(aes(x=product(SurvivedFac, PclassFac),fill=SurvivedFac)) +
@@ -375,7 +370,7 @@ ggplot(data=train) +
 ## ------------------------------------------------------------------------
 names(train)
 
-## ----fig.height=4.5, fig.width=8, echo=FALSE-----------------------------
+## ----fig.height=4.5, fig.width=8-----------------------------------------
 # 4 Survived and NameLength
 plot(train$SurvivedNum~train$NameLength, pch=19, col=rgb(0,0,.6,.2),
     main="Titanic Survival by Name Length",
@@ -386,7 +381,7 @@ g=glm(SurvivedNum~NameLength,family='binomial',data=train)
 curve(predict(g,data.frame(NameLength=x),type="resp"),col="red",lty=2,lwd=2,add=TRUE) 
 legend(60,0.5,c("linear fit","logistic fit"), col=c("green","red"), lty=c(1,2))
 
-## ----fig.height=5, fig.width=9, echo=FALSE-------------------------------
+## ----fig.height=5, fig.width=9-------------------------------------------
 # 5 Survival and Title
 ggplot(data=train) +
    geom_mosaic(aes(x=product(SurvivedFac, Title),fill=SurvivedFac)) + 
@@ -394,34 +389,34 @@ ggplot(data=train) +
    title='Tianic Survival by Title') + 
    theme(axis.text.x = element_text(angle = 90))
 
-## ----fig.height=4, fig.width=6, echo=FALSE-------------------------------
+## ----fig.height=4, fig.width=6-------------------------------------------
 # 6 Survived & Gender
 ggplot(data=train) +
    geom_mosaic(aes(x=product(SurvivedFac, GenderFac),fill=SurvivedFac)) +
    labs(x='Sex', y='', title='Titanic Survival by Gender')
 
-## ----fig.height=5, fig.width=8.5, echo=FALSE-----------------------------
+## ----fig.height=5, fig.width=8.5-----------------------------------------
 # 7 Survived and Sibling/Spouse
 ggplot(data=train) +
    geom_mosaic(aes(x=product(SurvivedFac, SiblingSpouse),fill=SurvivedFac)) +
    labs(x='Number of Siblings/Spouses', y='', 
    title='Titanic Survival by Number of Siblings or Spouses')
 
-## ----fig.height=5, fig.width=8.5, echo=FALSE-----------------------------
+## ----fig.height=5, fig.width=8.5-----------------------------------------
 # 8 Survived and Parent/Children
 ggplot(data=train) +
    geom_mosaic(aes(x=product(SurvivedFac, ParentChildren),fill=SurvivedFac)) +
    labs(x='Number of Parents/Children', y='', 
    title='Titanic Survival by Number of Parents or Children')
 
-## ----fig.height=5, fig.width=8.5, echo=FALSE-----------------------------
+## ----fig.height=5, fig.width=8.5-----------------------------------------
 # Survived and Number of Relatives
 ggplot(data=train) +
    geom_mosaic(aes(x=product(SurvivedFac, NumRelatives),fill=SurvivedFac)) +
    labs(x='Number of Parents/Children', y='', 
    title='Titanic Survival by Number of Relatives')
 
-## ----fig.height=5, fig.width=9, echo=FALSE-------------------------------
+## ----fig.height=5, fig.width=9-------------------------------------------
 # 9 Survived and Ticket Counts
 ticketcounts <- factor(train$TicketCount)
 ggplot(data=train) +
@@ -429,7 +424,7 @@ ggplot(data=train) +
    labs(x='Number of People Per Ticket', y='', 
    title='Titanic Survival by Size of Groups Per Ticket')
 
-## ----fig.height=5, fig.width=8.5, echo=FALSE-----------------------------
+## ----fig.height=5, fig.width=8.5-----------------------------------------
 # 10 Survived & Fare
 plot(train$SurvivedNum~train$FarePerPerson, pch=19, col=rgb(0,0,.6,.2),
     main="Titanic Survival by Fare",ylab="Probability of Survival", xlab="Fare (Pounds Sterling)")
@@ -439,7 +434,7 @@ g=glm(SurvivedNum~FarePerPerson,family='binomial',data=train)
 curve(predict(g,data.frame(FarePerPerson=x),type="resp"),col="red",lty=2,lwd=2,add=TRUE) 
 legend(120,0.7,c("linear fit","logistic fit"), col=c("green","red"), lty=c(1,2))
 
-## ----fig.height=5, fig.width=8.5, echo=FALSE-----------------------------
+## ----fig.height=5, fig.width=8.5-----------------------------------------
 # 10 Survived & Log(Fare)
 plot(train$SurvivedNum~train$FarePerPersonLog, pch=19, col=rgb(0,0,.6,.2),
     main="Titanic Survival by Log of Fare",
@@ -450,7 +445,7 @@ g=glm(SurvivedNum~FarePerPersonLog,family='binomial',data=train)
 curve(predict(g,data.frame(FarePerPersonLog=x),type="resp"),col="red",lty=2,lwd=2,add=TRUE) 
 legend(1,0.7,c("linear fit","logistic fit"), col=c("green","red"), lty=c(1,2))
 
-## ----fig.height=5, fig.width=8.5, echo=FALSE-----------------------------
+## ----fig.height=5, fig.width=8.5-----------------------------------------
 # 11 Survived & Age
 plot(train$SurvivedNum~train$AgeNum, pch=19, col=rgb(0,0,.6,.2),
     main="Titanic Survival by Age",ylab="Probability of Survival", xlab="Age")
@@ -460,7 +455,7 @@ g=glm(SurvivedNum~AgeNum,family='binomial',data=train)
 curve(predict(g,data.frame(AgeNum=x),type="resp"),col="red",lty=2,lwd=2,add=TRUE) 
 legend(60,0.7,c("linear fit","logistic fit"), col=c("green","red"), lty=c(1,2))
 
-## ----fig.height=5, fig.width=8.5, echo=FALSE-----------------------------
+## ----fig.height=5, fig.width=8.5-----------------------------------------
 # 11 Survived & Age as Factor
 ggplot(data=train) +
    geom_mosaic(aes(x=product(SurvivedFac, AgeFac),fill=SurvivedFac)) +
@@ -532,4 +527,8 @@ ggplot(data=train[train$PclassNum==3,]) +
    labs(x='', y='', 
    title='Survival of 3rd Class Passengers by Gender and Age Group') +
    coord_flip()
+
+## ----include=FALSE-------------------------------------------------------
+#library(knitr)
+#purl("Titanic_Survival.Rmd", output = "Rcode.R")
 
